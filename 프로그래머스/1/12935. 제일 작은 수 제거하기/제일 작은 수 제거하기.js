d986478960bfafa6e605a89db3b0c9adb01dd2ec
JIYOUNG <154482077+Zzzzyoung@users.noreply.github.com>
2024-01-07 18:28:42 +0900

    Update 제일 작은 수 제거하기.js

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,6 +1,9 @@
 function solution(arr) {
-    const minNumber = Math.min(...arr)
-    arr.splice(arr.indexOf(minNumber),1)
-    const answer = arr.length ? arr : [-1]
+    var answer = [];
+    var minNum = Math.min(...arr);
+    var idx = arr.indexOf(minNum);
+    
+    arr.splice(idx,1);
+    answer = (arr.length === 0) ? arr.push(-1) : arr;
     return answer;
 }
