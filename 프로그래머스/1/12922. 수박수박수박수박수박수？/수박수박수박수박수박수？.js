57466af2adffe27a5ccc5afff76c6a6024f37158
JIYOUNG <154482077+Zzzzyoung@users.noreply.github.com>
2024-01-08 11:48:07 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,13 +1,12 @@
 function solution(n) {
     var answer = '';
-    var arr = [];
+
     for (i = 0; i < n; i++) {
         if (i % 2 === 0) {
-            arr.push('수');
+            answer += '수';
         } else {
-            arr.push('박');
+            answer += '박';
         }
     }
-    answer = arr.join('');
     return answer;
 }
