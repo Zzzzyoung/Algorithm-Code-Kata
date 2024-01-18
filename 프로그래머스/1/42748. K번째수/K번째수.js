cefca57fde180edef0beaec7d1e09596cf2e0a14
JIYOUNG <154482077+Zzzzyoung@users.noreply.github.com>
2024-01-18 10:09:21 +0900

    [level 1] Title: K번째수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,11 @@
+function solution(array, commands) {
+    var answer = [];
+    var newArr = [];
+    
+    for (i = 0; i < commands.length; i ++) {
+        newArr = array.slice(commands[i][0]-1, commands[i][1]);
+        newArr.sort((a, b) => a - b);
+        answer.push(newArr[commands[i][2]-1]);
+    }
+    return answer;
+}
